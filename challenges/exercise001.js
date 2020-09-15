b3126374bf6427a9256522a07b39fdd04a7b6444
Ellie Maidment <ellie947@hotmail.com>
2020-09-15 10:53:43 +0100

    getMeanScores debugged - still failing test despite function returning typeof number instead of string

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,43 +1,46 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
   // Add your code here!
-  if (word[0] === word[0].toUpperCase()){
-    return word; 
+  if (word[0] === word[0].toUpperCase()) {
+    return word;
   } else {
-    return word.charAt(0).toUpperCase() + word.slice(1);  
+    return word.charAt(0).toUpperCase() + word.slice(1);
   }
 }
 
-capitalize('hello');
+capitalize("hello");
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
   // Add your code here!
   let fullName = `${firstName} ${lastName}`;
-  return fullName.split(" ").map((n)=>n[0]).join(".");
+  return fullName
+    .split(" ")
+    .map((n) => n[0])
+    .join(".");
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
   // Add your code here!
-  return Number((originalPrice/100)*(vatRate+100)).toFixed(2);
+  return Number((originalPrice / 100) * (vatRate + 100)).toFixed(2);
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
   // Add your code here!
-  return originalPrice - (originalPrice * (reduction / 100));
+  return originalPrice - originalPrice * (reduction / 100);
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
   // Add your code here!
-  if(str.length % 2 === 0){
-    return str[(str.length / 2) - 1] + str[str.length / 2];
-    } else {
+  if (str.length % 2 === 0) {
+    return str[str.length / 2 - 1] + str[str.length / 2];
+  } else {
     return str[Math.floor(str.length / 2)];
   }
 }
@@ -45,42 +48,45 @@ function getMiddleCharacter(str) {
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
   // Add your code here!
-  return word.split('').reverse().join('');
+  return word.split("").reverse().join("");
 }
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Add your code here!
-  return words.map(word => word.split('').reverse().join(''));
+  return words.map((word) => word.split("").reverse().join(""));
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
   // Add your code here!
-  let result = users.filter(function(user){
-    return user.type == "Linux"
+  let result = users.filter(function (user) {
+    return user.type == "Linux";
   });
   return result.length;
 }
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
-  // Add your code here!
-  return Number(scores.reduce((a,b) => a + b) / scores.length).toFixed(2);
+  let result = parseFloat(
+    (scores.reduce((a, b) => a + b) / scores.length).toFixed(2)
+  );
+  return result;
+  // Still failing tests, I just don't understand why. I debugged using typeof and it's returning a number not a string.
 }
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
   // Add your code here!
-  if (n % 3 == 0 && n % 5 == 0){
-    return "fizzbuzz"
-} else if (n % 3 == 0){
-  return "fizz";
-} else if (n % 5 == 0){
-  return "buzz";
-} else {
-  return n;
-}
+  if (n % 3 == 0 && n % 5 == 0) {
+    return "fizzbuzz";
+  } else if (n % 3 == 0) {
+    return "fizz";
+  } else if (n % 5 == 0) {
+    return "buzz";
+  } else {
+    return n;
+  }
 }
 
 module.exports = {
@@ -93,5 +99,5 @@ module.exports = {
   reverseAllWords,
   countLinuxUsers,
   getMeanScore,
-  simpleFizzBuzz
+  simpleFizzBuzz,
 };
