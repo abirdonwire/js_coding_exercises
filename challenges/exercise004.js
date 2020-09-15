b5d3f8e76f92f73834d6c5e2abe52e2eddade970
Ellie Maidment <ellie947@hotmail.com>
2020-09-15 15:29:42 +0100

    exercise004 getSquareRoots - complete

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -26,7 +26,7 @@ function getCities(users) {
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  return nums.map((value) => parseFloat(Math.sqrt(value).toFixed(2)));
 }
 
 function findSentencesContaining(sentences, str) {
