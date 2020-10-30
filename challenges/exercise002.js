b778742e112b013d009a989d7825ac2826a52b79
Ellie Maidment <ellie947@hotmail.com>
2020-10-30 15:44:54 +0000

    refactor hasMPostCode

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,35 +1,33 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+
   return sandwich.fillings;
 }
 
-
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
-  return person.city == "Manchester" ? true : false;
+
+  return person.city === "Manchester";
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+
   const seatsOnBus = 40;
-  let result = Math.ceil(people/seatsOnBus);
+  let result = Math.ceil(people / seatsOnBus);
   return result;
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
-  return arr.filter(x => x === "sheep").length;
+
+  return arr.filter((x) => x === "sheep").length;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
   let pCode = person.address.postCode;
-  return pCode.charAt(0) == 'M' && Number.isInteger(parseInt(pCode.charAt(1))) ? true : false;
+  return pCode.charAt(0) == "M" && Number.isInteger(parseInt(pCode.charAt(1)));
 }
 
 module.exports = {
@@ -37,5 +35,5 @@ module.exports = {
   isFromManchester,
   countSheep,
   getBusNumbers,
-  hasMPostCode
+  hasMPostCode,
 };
